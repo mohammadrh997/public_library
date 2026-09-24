eeebe82d2a0d496683f8f6cc926ec91011ee261b
mohammad <mohammadrh997@gmail.com>
2026-09-24 17:26:33 +0300

    feat: add_book, patch_book, and delete_book in books

--- app/routers/books.py
+++ app/routers/books.py
@@ -1,4 +1,5 @@
 from typing import Annotated
+import logging
 
 from fastapi import APIRouter, status, HTTPException, Depends
 from fastapi.concurrency import run_in_threadpool
@@ -15,7 +16,7 @@ from app.dependencies import DBsession, CurrentMember, require_librarian, pagina
 
 router = APIRouter(prefix="/books", tags=["books"])
 PaginationDep = Annotated[dict, Depends(pagination_params)]
-
+logger = logging.getLogger(__name__)
 
 
 async def fetch_book(book_id: int, db: AsyncSession):
@@ -40,3 +41,43 @@ async def get_books(db: DBsession, pagation: PaginationDep, search: None | str =
 async def get_book(book_id: int, db: DBsession):
     book = await fetch_book(book_id, db)
     return book
+
+
+@router.post("", response_model= BookRead, status_code=status.HTTP_201_CREATED, dependencies=[(Depends(require_librarian))])
+async def add_book(payload: BookCreate, db: DBsession):
+    book = Book(**payload.model_dump())
+    db.add(book)
+    try:
+        await db.commit()
+    except IntegrityError:
+        await db.rollback()
+        raise HTTPException(
+            status_code=409,
+            detail="the ISBN already exists",
+        )
+    await db.refresh(book)
+    return book
+
+
+@router.patch("/{book_id}", response_model=BookRead, dependencies=[(Depends(require_librarian))])
+async def patch_book(book_id: int, payload: BookUpdate, db: DBsession):
+    book = await fetch_book(book_id, db)
+    for field, value in payload.model_dump(exclude_unset=True).items():
+        setattr(book, field, value)
+    try:
+        await db.commit()
+    except IntegrityError as e:
+        await db.rollback()
+        logger.error(e)
+        raise HTTPException(
+            status_code=409,
+            detail="the ISBN already exists",
+        )
+    await db.refresh(book)
+    return book
+
+@router.delete("/{book_id}", dependencies=[(Depends(require_librarian))], status_code=status.HTTP_204_NO_CONTENT)
+async def delete_book(book_id: int, db: DBsession):
+    book = await fetch_book(book_id, db)
+    await db.delete(book)
+    await db.commit()
