9bcd48746e737bc4b0f7b9315aecf3fd28fe795c
mohammad <mohammadrh997@gmail.com>
2026-09-24 16:52:43 +0300

    feat: add get_book, fetch_book functions

--- app/routers/books.py
+++ app/routers/books.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter, status, HTTPException, Depends
 from fastapi.concurrency import run_in_threadpool
 
 from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.exc import IntegrityError
 
 from app.models import Book, Loan
@@ -15,7 +16,16 @@ from app.dependencies import DBsession, CurrentMember, require_librarian, pagina
 router = APIRouter(prefix="/books", tags=["books"])
 PaginationDep = Annotated[dict, Depends(pagination_params)]
 
-@router.get("/books", response_model=list[BookRead])
+
+
+async def fetch_book(book_id: int, db: AsyncSession):
+    book = await db.get(Book, book_id)
+    if not book:
+        raise HTTPException(status_code=404, detail="Not Found")
+    return book
+
+
+@router.get("", response_model=list[BookRead])
 async def get_books(db: DBsession, pagation: PaginationDep, search: None | str = None):
     stmt = select(Book).order_by(Book.id)
     if search:
@@ -25,3 +35,8 @@ async def get_books(db: DBsession, pagation: PaginationDep, search: None | str =
     if not books:
         raise HTTPException(status_code=404, detail="Not Found")
     return books
+
+@router.get("/{book_id}", response_model= BookRead)
+async def get_book(book_id: int, db: DBsession):
+    book = await fetch_book(book_id, db)
+    return book
